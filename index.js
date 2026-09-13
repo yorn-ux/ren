e1e41800cb9644b835e401c5d13a01c003bfaddf
yorn-ux <nyawandavincent4@gmail.com>
2026-09-13 17:05:36 +0300

    Initial commit: project skeleton

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+console.log('Ren is starting up...');
+
+// This will grow into the core engine:
+// - mode registry
+// - scheduler
+// - LLM connection
+// - dashboard API
